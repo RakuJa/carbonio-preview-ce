69ce15dba3d124d9c2f8038c3de8d39166371d80
Daniele Giachetto <daniele.giachetto@zextras.com>
2023-04-12 12:34:34 +0200

    chore: add staging path

--- test_package.sh
+++ test_package.sh
@@ -10,8 +10,9 @@ build_package()
   targetOs=$1
   targetRegistry=$2
   pkgVersion=$3
+  stagingPath=$4
   echo "Building package for" "$targetOs"", this may take a while.."
-  docker run --rm --entrypoint ""  --mount type=bind,source="$(pwd)"/../staging,target=/tmp/staging -e VERSION="$pkgVersion" $targetRegistry /bin/bash -c 'cd /tmp/staging/package && pacur build '"$targetOs"
+  docker run --rm --entrypoint ""  --mount type=bind,source="$stagingPath",target=/tmp/staging -e VERSION="$pkgVersion" $targetRegistry /bin/bash -c 'cd /tmp/staging/package && pacur build '"$targetOs"
 }
 
 install_package()
@@ -20,6 +21,7 @@ install_package()
   pkgVer=$2
   pkgRel=$3
   targetOs=$4
+  stagingPath=$5
   if [ "$targetOs" = "Rocky" ]; then
     pkgExt=".el8.x86_64.rpm"
     targetRegistry=registry.dev.zextras.com/jenkins/pacur/rocky-8:v1
@@ -32,15 +34,16 @@ install_package()
     install_command='apt install /tmp/'"$pkgFullName"
   fi
 
-  mountPath="/../staging/package/""$pkgFullName"
-  docker run --rm --entrypoint "" --mount type=bind,source="$(pwd)""$mountPath",target=/tmp/"$pkgFullName" $targetRegistry bin/bash -c "$install_command"
+  mountPath="$stagingPath/package/$pkgFullName"
+  docker run --rm --entrypoint "" --mount type=bind,source="$mountPath",target=/tmp/"$pkgFullName" $targetRegistry bin/bash -c "$install_command"
 }
 
 set_package_version()
 {
   vers=$1
   rel=$2
-  file=$3
+  stagingPath=$3
+  file="$stagingPath/package/PKGBUILD"
 
   cat "$file" | sed -e "s/\(pkgver=\).*/\1\"$vers\"/" -e "s/\(pkgrel=\).*/\1\"$rel\"/" > "$(pwd)"/../staging/tempPKG
   mv "$(pwd)"/../staging/tempPKG "$file"
@@ -53,13 +56,15 @@ version=1
 releases=1
 help=false
 no_cleanup=false
-while getopts urnhiv: flag
+project_path=$(pwd)
+while getopts ruinhv:p: flag
 do
     case "${flag}" in
         u) ubuntu=true;;
         r) rocky8=true;;
         i) install=true;;
         v) version=${OPTARG};;
+        p) project_path=${OPTARG};;
         h) help=true;;
         n) no_cleanup=true;;
         *) help=true;;
@@ -75,26 +80,28 @@ if [ $help = true ]; then
    exit 0
  fi
 
+echo "project path" "$project_path";
 echo "ubuntu: $ubuntu";
 echo "rocky8: $rocky8";
 echo "install": $install;
 echo "version: $version";
 echo "no cleanup": $no_cleanup
 
+staging_path="$project_path"/../staging
 
 buildUbuntuPackage=1
-cp -r "$(pwd)" "$(pwd)"/../staging
+cp -r "$project_path" "$staging_path"
 
-set_package_version "$version" "$releases" "$(pwd)"/../staging/package/PKGBUILD
+set_package_version "$version" "$releases" "$staging_path"
 
 if [ $ubuntu = true ]; then
-  build_package "ubuntu" registry.dev.zextras.com/jenkins/pacur/ubuntu-20.04:v1 "$version"
+  build_package "ubuntu" registry.dev.zextras.com/jenkins/pacur/ubuntu-20.04:v1 "$version" "$staging_path"
   buildUbuntuPackage=$?
 fi
 
 buildRockyPackage=1
 if [ $rocky8 = true ]; then
-  build_package "rocky" registry.dev.zextras.com/jenkins/pacur/rocky-8:v1 "$version"
+  build_package "rocky" registry.dev.zextras.com/jenkins/pacur/rocky-8:v1 "$version" "$staging_path"
   buildRockyPackage=$?
 fi
 
@@ -104,7 +111,7 @@ if [ $buildUbuntuPackage = 0 ]; then
   echo "Ubuntu build completed successfully"
   if [ $install = true ]; then
     echo "Installing package for ubuntu.."
-    install_package "carbonio-preview-ce" "$version" "$releases" "Ubuntu"
+    install_package "carbonio-preview-ce" "$version" "$releases" "Ubuntu" "$staging_path"
     installUbuntuPackage=$?
   fi
 fi
@@ -114,7 +121,7 @@ if [ $buildRockyPackage = 0 ]; then
   echo "Rocky build completed successfully"
   if [ $install = true ]; then
     echo "Installing package for rocky8.."
-    install_package "carbonio-preview-ce" "$version" "$releases" "Rocky"
+    install_package "carbonio-preview-ce" "$version" "$releases" "Rocky" "$staging_path"
     installRockyPackage=$?
   fi
 fi
@@ -131,6 +138,6 @@ echo "Summary:
 
 if [ $no_cleanup = false ]; then
   echo "Cleaning up.. "
-  rm -rf "$(pwd)"/../staging
+  rm -rf "$staging_path"
 fi
 exit 0
